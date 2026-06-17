d83a61e5c9e1e38eecd8f17348f04351d5dee82b
denkz0ne <58009596+denkz0ne@users.noreply.github.com>
2026-06-17 07:00:04 +0200

    Bump materialDetector core version to 5.0.1

--- materialDetector.user.js
+++ materialDetector.user.js
@@ -2,7 +2,7 @@
 // @name         materialDetector
 // @namespace    https://moduly.faxcopy.sk/
 // @author       mato e.
-// @version      5.0.0-core
+// @version      5.0.1-core
 // @description  Material detector core router + modular internal-code detectors.
 // @updateURL    https://github.com/denkz0ne/moduly-FC-userscripts/raw/codex/materialdetector-core/materialDetector.user.js
 // @downloadURL  https://github.com/denkz0ne/moduly-FC-userscripts/raw/codex/materialdetector-core/materialDetector.user.js
